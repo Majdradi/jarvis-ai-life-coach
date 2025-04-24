c15e81d5584d69c1223767bf50f1cde392c987f2
Majdradi <90189007+Majdradi@users.noreply.github.com>
2025-04-24 19:30:35 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
+// Root-level build.gradle file
+
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.0.2'  // Make sure this is the right version
-        classpath 'com.google.gms:google-services:4.3.10'  // If you're using Firebase
+        // Classpath for the Android Gradle plugin
+        classpath 'com.android.tools.build:gradle:7.0.2'  // Update to the latest version if necessary
+        // Firebase or Google Services if needed
+        classpath 'com.google.gms:google-services:4.3.10'  // Update to the latest version if necessary
     }
 }
 
@@ -15,3 +19,4 @@ allprojects {
         mavenCentral()
     }
 }
+
